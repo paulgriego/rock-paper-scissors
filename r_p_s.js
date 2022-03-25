7d87bd9b417430f5ecc666542994ba657dd01c64
Paul Griego <paulgriego029@gmail.com>
2022-03-25 15:32:58 -0600

    adding if else if for user selection vs computer

--- r_p_s.js
+++ r_p_s.js
@@ -6,5 +6,26 @@ function computerPlay() {
 userInput=window.prompt("Lets play rock, paper, scissors! Enter your choice:");
 userInputCaseInsenstive=userInput.toLowerCase();
 computerSelection=computerPlay()
-console.log(userInputCaseInsenstive)
 
+
+if (userInputCaseInsenstive=="rock" && computerSelection=="rock") {
+  alert(`The computer picks:` + (computerSelection)+ ' It is a tie!');
+} else if  (userInputCaseInsenstive=="rock" && computerSelection=="scissors") {
+    alert(`The computer picks:` + (computerSelection)+ ` Congratulations you win! Rock beats Scissors!`);
+} else if  (userInputCaseInsenstive=="rock" && computerSelection=="paper") {
+    alert(`The computer picks:` + (computerSelection)+ ` Sorry you lost! Paper beats rock!`);  
+} else if  (userInputCaseInsenstive=="scissors" && computerSelection=="scissors") {
+  alert(`The computer picks:` + (computerSelection)+ ` It is a tie!`);
+} else if  (userInputCaseInsenstive=="scissors" && computerSelection=="paper") {
+  alert(`The computer picks:` + (computerSelection)+ ` Congratulations you win! Scissors beats paper!`);
+} else if  (userInputCaseInsenstive=="scissors" && computerSelection=="rock") {
+  alert(`The computer picks:` + (computerSelection)+ ` Sorry you lost! Rocks beats Scissors!`);
+} else if  (userInputCaseInsenstive=="paper" && computerSelection=="paper") {
+  alert(`The computer picks:` + (computerSelection)+ ` It is a tie!`);
+} else if  (userInputCaseInsenstive=="paper" && computerSelection=="scissors") {
+  alert(`The computer picks:` + (computerSelection)+ `Sorry you lost! Scissors beats paper!`);
+} else if  (userInputCaseInsenstive=="paper" && computerSelection=="rock") {
+  alert(`The computer picks:` + (computerSelection)+ `Congratulations you won! Paper beats rock!`);
+} else {
+  alert("You didn't choose rock,paper,or scissors! Try again")
+}
