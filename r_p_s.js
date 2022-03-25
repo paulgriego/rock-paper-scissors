b7b2e7282e97b60ecbfda8075fff2e4b9ab4384c
Paul Griego <paulgriego029@gmail.com>
2022-03-25 14:45:46 -0600

    added function for selction random string and userinput w case insenstive

--- r_p_s.js
+++ r_p_s.js
@@ -1,1 +1,10 @@
-console.log('test')
+function computerPlay() {
+  let string = ["rock","paper","scissors"];
+  let computerOutput = string[Math.floor(Math.random()*string.length)]; 
+  return computerOutput
+}
+userInput=window.prompt("Lets play rock, paper, scissors! Enter your choice:");
+userInputCaseInsenstive=userInput.toLowerCase();
+computerSelection=computerPlay()
+console.log(userInputCaseInsenstive)
+
