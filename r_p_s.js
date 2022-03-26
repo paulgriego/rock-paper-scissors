9baa808b5f7729e22e89492bc5e42ba239c678e7
Paul Griego <paulgriego029@gmail.com>
2022-03-26 13:04:14 -0600

    Updating js to watch out for if user enters a space

--- r_p_s.js
+++ r_p_s.js
@@ -1,31 +1,38 @@
+// The function computer play takes the array, and chooses the three array choices at random.
+
+
 function computerPlay() {
   let string = ["rock","paper","scissors"];
   let computerOutput = string[Math.floor(Math.random()*string.length)]; 
   return computerOutput
 }
+
+
 userInput=window.prompt("Lets play rock, paper, scissors! Enter your choice:");
-userInputCaseInsenstive=userInput.toLowerCase();
-computerSelection=computerPlay()
+let userLowerCase=userInput.toLowerCase();
+let userInputfinal=userLowerCase.trim();
+let computerSelection=computerPlay();
 
 
-if (userInputCaseInsenstive=="rock" && computerSelection=="rock") {
+if (userInputfinal=="rock" && computerSelection=="rock") {
   alert(`The computer picks:` + (computerSelection)+ ' It is a tie!');
-} else if  (userInputCaseInsenstive=="rock" && computerSelection=="scissors") {
+} else if  (userInputfinal=="rock" && computerSelection=="scissors") {
     alert(`The computer picks:` + (computerSelection)+ ` Congratulations you win! Rock beats Scissors!`);
-} else if  (userInputCaseInsenstive=="rock" && computerSelection=="paper") {
+} else if  (userInputfinal=="rock" && computerSelection=="paper") {
     alert(`The computer picks:` + (computerSelection)+ ` Sorry you lost! Paper beats rock!`);  
-} else if  (userInputCaseInsenstive=="scissors" && computerSelection=="scissors") {
+} else if  (userInputfinal=="scissors" && computerSelection=="scissors") {
   alert(`The computer picks:` + (computerSelection)+ ` It is a tie!`);
-} else if  (userInputCaseInsenstive=="scissors" && computerSelection=="paper") {
+} else if  (userInputfinal=="scissors" && computerSelection=="paper") {
   alert(`The computer picks:` + (computerSelection)+ ` Congratulations you win! Scissors beats paper!`);
-} else if  (userInputCaseInsenstive=="scissors" && computerSelection=="rock") {
+} else if  (userInputfinal=="scissors" && computerSelection=="rock") {
   alert(`The computer picks:` + (computerSelection)+ ` Sorry you lost! Rocks beats Scissors!`);
-} else if  (userInputCaseInsenstive=="paper" && computerSelection=="paper") {
+} else if  (userInputfinal=="paper" && computerSelection=="paper") {
   alert(`The computer picks:` + (computerSelection)+ ` It is a tie!`);
-} else if  (userInputCaseInsenstive=="paper" && computerSelection=="scissors") {
+} else if  (userInputfinal=="paper" && computerSelection=="scissors") {
   alert(`The computer picks:` + (computerSelection)+ ` Sorry you lost! Scissors beats paper!`);
-} else if  (userInputCaseInsenstive=="paper" && computerSelection=="rock") {
+} else if  (userInputfinal=="paper" && computerSelection=="rock") {
   alert(`The computer picks:` + (computerSelection)+ ` Congratulations you won! Paper beats rock!`);
 } else {
   alert("You didn't choose rock,paper,or scissors! Try again")
 }
+
