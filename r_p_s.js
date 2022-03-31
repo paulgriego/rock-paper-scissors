cd4b35f6601640d28cf53340b47fb406d4dbd3bf
Paul Griego <paulgriego029@gmail.com>
2022-03-31 01:29:08 -0600

    adding variables to keep track of how many games, and score for each player

--- r_p_s.js
+++ r_p_s.js
@@ -17,39 +17,75 @@ let userInput
     return userInput="scissors";
   }
 }
+
 //This compares the userInput vs the ComputerPlay and outputs the result of the game.
+ 
+ 
+ let i = 1
+ let playerscore = 1
+ let computerscore = 1
  function playRound(x, y ) {
-     
+   
    if (x=="rock" && y=="rock") {
     console.log("The computer picks rock it is a tie!");
+    console.log(i);
     }
    else if  (x=="rock" && y=="paper") {
     console.log("The computer picked paper you lost");
+    console.log(i);
+    console.log(computerscore);
+    i++;
+    computerscore++;
     }
    else if  (x=="rock" && y=="scissors") {
     console.log("The computer picked scissors you won");
+    console.log(i);
+    console.log(playerscore);
+    i++;
+    playerscore ++;
+
     }
    else if  (x=="paper" && y=="scissors") {
     console.log("The computer picked scissors you lost");
+    console.log(i);
+    console.log(computerscore);
+    i++;
+    computerscore++;
     }
    else if (x=="paper" && y=="rock") {
     console.log("The computer picks rock you won!");
+    console.log(i);
+    console.log(playerscore);
+    i++;
+    playerscore ++;
     }
    else if  (x=="paper" && y=="paper") {
     console.log("The computer picked paper its a tie");
+    console.log(i);
     }
    else if  (x=="scissors" && y=="scissors") {
       console.log("The computer picked scissors its a tie");
+      console.log(i);
       }
    else if (x=="scissors" && y=="rock") {
       console.log("The computer picks rock you lost!");
+      console.log(i);
+      console.log(computerscore);
+      i++;
+      computerscore++;
       }
    else if  (x=="scissors" && y=="paper") {
       console.log("The computer picked paper you won");
+      console.log(i);
+      console.log(playerscore);
+      i++;
+      playerscore ++;
       }
+    }
+
    
   
-    }
+  
   
 
  
