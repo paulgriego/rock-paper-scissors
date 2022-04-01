366577bb641a5401c99840f3fb8ae7ab6e72baa8
Paul Griego <paulgriego029@gmail.com>
2022-03-31 22:38:20 -0600

    updated functions, added stylesheets

--- r_p_s.js
+++ r_p_s.js
@@ -18,70 +18,106 @@ let userInput
   }
 }
 
+
+
 //This compares the userInput vs the ComputerPlay and outputs the result of the game.
  
  
- let i = 1
- let playerscore = 1
- let computerscore = 1
+ let i = 0
+ let playerscore = 0
+ let computerscore = 0
+
  function playRound(x, y ) {
    
    if (x=="rock" && y=="rock") {
-    console.log("The computer picks rock it is a tie!");
-    console.log(i);
+    document.getElementById('Result').innerHTML="The computer picks rock it is a tie!";
+    document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+    document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
     }
    else if  (x=="rock" && y=="paper") {
-    console.log("The computer picked paper you lost");
-    console.log(i);
-    console.log(computerscore);
     i++;
     computerscore++;
+    document.getElementById('Result').innerHTML="The computer picked paper you lost";
+    document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+    document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
     }
    else if  (x=="rock" && y=="scissors") {
-    console.log("The computer picked scissors you won");
-    console.log(i);
-    console.log(playerscore);
     i++;
-    playerscore ++;
-
+    playerscore++; 
+    document.getElementById('Result').innerHTML="The computer picked scissors you won";
+    document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+    document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
     }
    else if  (x=="paper" && y=="scissors") {
-    console.log("The computer picked scissors you lost");
-    console.log(i);
-    console.log(computerscore);
     i++;
-    computerscore++;
+    computerscore++ 
+    document.getElementById('Result').innerHTML="The computer picked scissors you lost";
+    document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+    document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
     }
    else if (x=="paper" && y=="rock") {
-    console.log("The computer picks rock you won!");
-    console.log(i);
-    console.log(playerscore);
     i++;
     playerscore ++;
+    document.getElementById('Result').innerHTML="The computer picks rock you won!";
+    document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+    document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
     }
    else if  (x=="paper" && y=="paper") {
-    console.log("The computer picked paper its a tie");
-    console.log(i);
+      document.getElementById('Result').innerHTML="The computer picked paper its a tie";
+      document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+      document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
     }
    else if  (x=="scissors" && y=="scissors") {
-      console.log("The computer picked scissors its a tie");
-      console.log(i);
+      document.getElementById('Result').innerHTML="The computer picked scissors its a tie";
+      document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+      document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
       }
    else if (x=="scissors" && y=="rock") {
-      console.log("The computer picks rock you lost!");
-      console.log(i);
-      console.log(computerscore);
       i++;
       computerscore++;
-      }
+      document.getElementById('Result').innerHTML="The computer picks rock you lost!";
+      document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+      document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
+   }
+      
    else if  (x=="scissors" && y=="paper") {
-      console.log("The computer picked paper you won");
-      console.log(i);
-      console.log(playerscore);
       i++;
       playerscore ++;
-      }
+      document.getElementById('Result').innerHTML="The computer picked paper you won";
+      document.getElementById('Score').innerHTML=`The computer score is: ` + (computerscore)+ ` The player score is: ` + (playerscore);
+      document.getElementById('RoundsPlayed').innerHTML=`Rounds Played: `+ (i);
+      } 
     }
+    
+  function Diablebutton() {
+    if (i==5) {
+     document.getElementById("r").disabled = true;
+     document.getElementById("p").disabled = true;
+     document.getElementById("s").disabled = true;
+     document.getElementById('GameOver').innerHTML='You have played five rounds!'  
+    }
+  }
+
+  function WhoWon() {
+    if (playerscore == 5 && computerscore ==0) {
+      document.getElementById("Winner").innerHTML='You beat the computer';
+    
+    } else if  (playerscore == 4 && computerscore ==1) {
+      document.getElementById("Winner").innerHTML='You beat the computer';
+    } else if  (playerscore == 3 && computerscore ==2) {
+      document.getElementById("Winner").innerHTML='You beat the computer';
+    
+    } else if  (computerscore == 5 && playerscore ==0) {
+      document.getElementById("Winner").innerHTML='The computer beat you';
+    
+    } else if  (computerscore == 4 && playerscore ==1) {
+      document.getElementById("Winner").innerHTML='The computer beat you';
+    
+    } else if  (computerscore == 3 && playerscore ==2) {
+      document.getElementById("Winner").innerHTML='The computer beat you';
+  }
+}
+
 
    
   
